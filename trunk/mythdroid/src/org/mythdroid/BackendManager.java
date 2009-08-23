28e3bd7d1a6e033f8c698c70ce49ce41226858ca
foobum <foobum@670a33b6-7f95-11de-a66a-778c95663ba5>
2009-08-23 20:09:07 +0000

    Add missing licence / copyright notices

--- trunk/mythdroid/src/org/mythdroid/BackendManager.java
+++ trunk/mythdroid/src/org/mythdroid/BackendManager.java
@@ -1,5 +1,6 @@
 /*
     MythDroid: Android MythTV Remote
+    Copyright (C) 2009-2010 foobum@gmail.com
     
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
