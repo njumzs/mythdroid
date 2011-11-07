4d3d689a59c22eff78fbff09e7e53874bf3677e1
foobum <foobum@670a33b6-7f95-11de-a66a-778c95663ba5>
2011-11-07 14:45:08 +0000

    Parse out the fps from a frontend playback location, we'll need this later

--- src/org/mythdroid/frontend/FrontendLocation.java
+++ src/org/mythdroid/frontend/FrontendLocation.java
@@ -31,7 +31,7 @@ public class FrontendLocation {
     @SuppressWarnings("all")
     public String  location  = null, niceLocation = null;
     @SuppressWarnings("all")
-    public int     position, end;
+    public int     position, end, fps = 0;
     @SuppressWarnings("all")
     public float   rate;
     @SuppressWarnings("all")
@@ -126,6 +126,7 @@ public class FrontendLocation {
                     tok[5].substring(0, tok[5].lastIndexOf('x'))
                 );
             filename = tok[9];
+            fps = Integer.valueOf(tok[10]);
             if (tok[1].equals("livetv")) livetv = true; //$NON-NLS-1$
         }
         else if (tok[1].equals("video")) { //$NON-NLS-1$
@@ -137,6 +138,7 @@ public class FrontendLocation {
                     tok[3].substring(0, tok[3].lastIndexOf('x'))
                 );
             filename = "Video"; //$NON-NLS-1$
+            fps = Integer.valueOf(tok[6]);
         }
         else {
             rate = loc.contains("pause") ? 0 : -1; //$NON-NLS-1$
