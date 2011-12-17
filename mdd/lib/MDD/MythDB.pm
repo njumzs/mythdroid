0ee794bca77ee7ba2a35121b474e95fc4dfcd226
foobum <foobum@670a33b6-7f95-11de-a66a-778c95663ba5>
2011-12-17 20:39:08 +0000

    MDD: Ensure that storage group paths have trailing slashes

--- mdd/lib/MDD/MythDB.pm
+++ mdd/lib/MDD/MythDB.pm
@@ -268,6 +268,7 @@ sub getStorGroups() {
         $getStorGroupsSth = execute($getStorGroupsSth, \$getStorGroupsSQL);
 
         while (my $aref = $getStorGroupsSth->fetchrow_arrayref) {
+            $aref[1] .= '/' unless $aref[1] =~ /\/$/;
             push @{ $storageGroups{$aref->[0]} }, $aref->[1];
         }
     
