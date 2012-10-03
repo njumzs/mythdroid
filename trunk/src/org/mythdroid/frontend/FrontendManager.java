b76c295ec1ff8f109572d690521f257d87100c86
foobum <foobum@670a33b6-7f95-11de-a66a-778c95663ba5>
2012-10-03 16:48:44 +0000

    Remove duplicate check for invalid jump locations from FrontendManager

--- trunk/src/org/mythdroid/frontend/FrontendManager.java
+++ trunk/src/org/mythdroid/frontend/FrontendManager.java
@@ -98,11 +98,6 @@ public class FrontendManager {
             throw new IllegalArgumentException(
                 Messages.getString("FrontendManager.3") //$NON-NLS-1$
             );
-        /* This isn't a valid location for the jump command but might have
-           been saved in Globals.lastLocation, little choice but to jump to the
-           main menu instead */ 
-        if (loc.location.endsWith(".xml")) //$NON-NLS-1$
-            return jumpTo("mainmenu"); //$NON-NLS-1$
         return jumpTo(loc.location.toLowerCase());
     }
 
